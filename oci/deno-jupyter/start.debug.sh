d176843df6185f5a842e1178aea3c17f567df3e9
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
2024-03-08 10:02:00 +0100

    test ffi fix tryout nb1

---
+++ oci/deno-jupyter/start.debug.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export PATH="$PATH:$HOME/.cargo/bin"
+source ~/.bashrc
+
+pwd
+
+cargo --version
+
+
+deno --version
+deno jupyter --install --unstable-ffi --allow-ffi --allow-env
+
+
+
+
+# tested works # jupyter notebook --unstable true --ip 0.0.0.0 --allow-root
+export NOTEBOOKS_DIR=$DATAVIZ_NOTEBOOKS_HOME
+jupyter lab --unstable=true --ip=0.0.0.0 --allow-root --NotebookApp.token='decoderleco' --NotebookApp.password='decoderleco' --notebook-dir="$NOTEBOOKS_DIR"
