636825f8269a3f11702ccc31b338effed78f777f
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
2024-03-08 11:07:43 +0100

    test ffi fix tryout nb4

--- oci/deno-jupyter/start.debug.sh
+++ oci/deno-jupyter/start.debug.sh
@@ -11,9 +11,11 @@ cargo --version
 deno --version
 # KO # deno jupyter --install --unstable-ffi --allow-ffi --allow-env
 # KO # deno jupyter --install --unstable-ffi --allow-env
-deno jupyter --install --unstable-ffi --unstable --unstable-fs --unstable-worker-options
+# OK but does not fix [ffi dl open] error # deno jupyter --install --unstable-ffi --unstable --unstable-fs --unstable-worker-options
+deno jupyter --install --unstable-ffi --unstable-fs # without [--unstable] which is deprecated for [--unstable-*] options
 # deno jupyter --install --unstable-ffi
 
 # tested works # jupyter notebook --unstable true --ip 0.0.0.0 --allow-root
 export NOTEBOOKS_DIR=$DATAVIZ_NOTEBOOKS_HOME
-jupyter lab --unstable=true --ip=0.0.0.0 --allow-root --NotebookApp.token='decoderleco' --NotebookApp.password='decoderleco' --notebook-dir="$NOTEBOOKS_DIR"
+# OK # jupyter lab --unstable=true --ip=0.0.0.0 --allow-root --NotebookApp.token='decoderleco' --NotebookApp.password='decoderleco' --notebook-dir="$NOTEBOOKS_DIR"
+jupyter lab --unstable=true --unstable-ffi=true --unstable-fs=true --ip=0.0.0.0 --allow-root --NotebookApp.token='decoderleco' --NotebookApp.password='decoderleco' --notebook-dir="$NOTEBOOKS_DIR"
